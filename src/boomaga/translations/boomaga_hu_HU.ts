8366ed3eb6b9e81166a7f3acc6037b476c1e44f3
Your Name <you@example.com>
2019-02-13 18:22:16 +0100

    TS fix by blackPanther OS

--- src/boomaga/translations/boomaga_hu_HU.ts
+++ src/boomaga/translations/boomaga_hu_HU.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS>
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="hu" version="2.1">
     <context>
         <name>AboutDialog</name>
         <message>
